66f5c143db50de275b79b9781339ceb3360f7f0f
Ane Sanchiz <ane.sanchiz@opendeusto.es>
2020-05-17 17:39:26 +0200

    +

--- miProyecto/miApp/templates/js.js
+++ miProyecto/miApp/templates/js.js
@@ -1,4 +1,36 @@
-const URL = "http://127.0.0.1:8000/miApp/pedidosjs/<int:pk>/";
+// IMAGENES PRODUCTOS
+let enlaces = document.getElementsByTagName('a')
+// Paso 2: itera por el listado de enlaces añadiendo un controlador para el evento
+for(el of enlaces){
+  el.addEventListener('click', actualizarImagenPrincipal);
+}
+// Paso 3: El controlador deberá cambiar la imagen principal accediendo a sus atributos
+function actualizarImagenPrincipal(event){
+  event.preventDefault(); // detener la navegación
+  let imagen = document.getElementById('imagen-seleccionada');
+  // Cambiar el atributo src de la imagen, poniendo el del enlace clickado
+  imagen.src = event.currentTarget.href;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const URL = "http://127.0.0.1:8000/miApp/pedidosjs/";
 
 //Botón [+] para ver detalles
 let masBtn = document.getElementById("btn-js-mas");
@@ -8,7 +40,7 @@ masBtn.addEventListener("click", (event) => {
 });
 
 
-function loadData() {
+function loadData(URL) {
   fetch(URL)
     .then((response) => response.json())
     .then((json) => {
